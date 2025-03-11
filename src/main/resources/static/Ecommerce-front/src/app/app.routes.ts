449ba5f76e98a5233b7054519d1ab80fe307184d
Guilherme-Dias-gomes <guilhermediasgomes2@gmail.com>
2025-03-11 17:56:14 -0300

    criando tela de cadastro

--- src/main/resources/static/Ecommerce-front/src/app/app.routes.ts
+++ src/main/resources/static/Ecommerce-front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component'; // Importe o novo componente
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
 { path: '', component: AppComponent, canActivate: [authGuard] }, // Rota protegida
 { path: 'login', component: AppComponent },
+{ path: 'cadastro', component: CadastroComponent }, // Nova rota para cadastro
 { path: '**', redirectTo: '' } // Rota de fallback
 ];
